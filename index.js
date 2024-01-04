9da9f358f5737e733d8f759579246de8b0f77c19
Karra <karra.maynard@gmail.com>
2024-01-04 16:32:06 -0500

    create async generateLogo()

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const fs = require('fs');
 const {Circle, Triangle, Square} = require('./lib/shapes');
 const {collectData} = require('./lib/interface');
+
+async function generateLogo() {
+  try {
+    // Wait for the data
+    const userData = await collectData();
+    console.log(userData);
+
+    //return result
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+generateLogo();
